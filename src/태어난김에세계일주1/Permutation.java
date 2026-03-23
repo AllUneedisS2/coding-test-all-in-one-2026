8f324d35ad1c250b984585b3a1da4ebc06a5693d
AllUneedisS2 <bmdk594@naver.com>
2026-03-23 17:11:22 +0900

    20260323-1711

--- src/태어난김에세계일주1/Permutation.java
+++ src/태어난김에세계일주1/Permutation.java
@@ -13,22 +13,36 @@ public class Permutation {
 
     }
 
+    private static int result = 0;
+
     private static int solution(int balance, int[][] countries) {
 
         int n = countries.length;
         boolean[] visited = new boolean[n];
         
         // 깊이 우선 탐색 + 순열 + BF
-        return dfs(balance, countries, 0, visited);
+        dfs(balance, countries, 0, visited);
 
+        return result;
     }
 
-    private static int dfs(int balance, int[][] countries, int count, boolean[] visited) {
+    private static void dfs(int balance, int[][] countries, int count, boolean[] visited) {
 
-        if 
+        result = Math.max(result, count);
 
+        for (int i = 0 ; i < countries.length ; i++) {
 
-        return 0;
+            // 방문 O
+            if(!visited[i] && balance >= countries[i][1]) {
+                count++;
+                visited[i] = true;
+                dfs(balance - countries[i][0], countries, count, visited);
+                count--;
+                visited[i] = false;
+            }
+
+        }
+
     }
 
 
