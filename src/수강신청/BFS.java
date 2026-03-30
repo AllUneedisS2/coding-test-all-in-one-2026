1db8375414d4bff15c5691ac4625772a3b088800
AllUneedisS2 <bmdk594@naver.com>
2026-03-30 19:42:54 +0900

    20260330-1942

---
+++ src/수강신청/BFS.java
@@ -0,0 +1,15 @@
+package 수강신청;
+
+public class BFS {
+
+    public static void main(String[] args) {
+
+        
+        System.out.println(solution());
+    }
+
+    static boolean solution(int numCourses, int[][] prerequisites) {
+
+    }
+
+}
